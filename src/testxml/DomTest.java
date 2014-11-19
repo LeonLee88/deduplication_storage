4f5a9a4b68a8eb99c8535cc2f3095381899fc846
goldenfish91 <yuyan91@bu.edu>
2014-11-18 20:14:11 -0500

    wirte xml

--- src/testxml/DomTest.java
+++ src/testxml/DomTest.java
@@ -1,4 +1,5 @@
 package testxml;
+
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
@@ -23,71 +24,73 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Text;
+
 public class DomTest {
- 
- // 写入ｘｍｌ文件
- public static void callWriteXmlFile(Document doc, Writer w, String encoding) {
-  try {
-   Source source = new DOMSource(doc);
-   Result result = new StreamResult(w);
-   Transformer xformer = TransformerFactory.newInstance()
-     .newTransformer();
-  // xformer.setOutputProperty(OutputKeys.ENCODING, encoding);
-   xformer.transform(source, result);
-  } catch (TransformerConfigurationException e) {
-   e.printStackTrace();
-  } catch (TransformerException e) {
-   e.printStackTrace();
-  }
- }
- public static void writeFileHash(String filename,ArrayList<String> hashlist) throws Exception {
+
+	// 写入ｘｍｌ文件
+	public static void callWriteXmlFile(Document doc, Writer w, String encoding) {
+		try {
+			Source source = new DOMSource(doc);
+			Result result = new StreamResult(w);
+			Transformer xformer = TransformerFactory.newInstance()
+					.newTransformer();
+			// xformer.setOutputProperty(OutputKeys.ENCODING, encoding);
+			xformer.transform(source, result);
+		} catch (TransformerConfigurationException e) {
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void writeFileHash(String fileId, ArrayList<String> hashlist)
+			throws Exception {
 
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		  DocumentBuilder builder = null;
-		  try {
-		   builder = dbf.newDocumentBuilder();
-		  } catch (Exception e) {
-		  }
-	      Document doc = builder.newDocument();
-		   
-		   //build filename tag
-		   Element name  = doc.createElement("name");
-		   name.setAttribute("id", filename);
-		   doc.appendChild(name);
-		   //build chunk tag
-		   Element chunk=doc.createElement("chunk");
-		   name.appendChild((Node) chunk);
-		   
-		   //loop for build hash tag
-		   for(int temp=0;temp<hashlist.size();temp++){
-			   Element hash=doc.createElement("hash");
-			   Text hashtext=doc.createTextNode(hashlist.get(temp));
-			   hash.appendChild(hashtext);
-			   chunk.appendChild(hash);
-			   
-		   }
-		   //out put
-		   try {
-			   FileOutputStream fos = new FileOutputStream(filename);
-			   OutputStreamWriter outwriter = new OutputStreamWriter(fos);
-			   
-			   callWriteXmlFile(doc, outwriter, "gb2312");
-			   outwriter.close();
-			   fos.close();
-			  } catch (Exception e) {
-			   e.printStackTrace();
-			  }
-			 }
- public static void main(String args[]) throws Exception {
-  String str = "myfile.xml";
-  DomTest t = new DomTest();
-  ArrayList<String> hashlist=new ArrayList<String>();
-	hashlist.add("aaaa");
-	hashlist.add("bbbb");
-	hashlist.add("cccc");
-	hashlist.add("dddd");
-	//writeFileHash("myfile", hashlist);
-    t.writeFileHash(str, hashlist);
- }
-}
+		DocumentBuilder builder = null;
+		try {
+			builder = dbf.newDocumentBuilder();
+		} catch (Exception e) {
+		}
+		Document doc = builder.newDocument();
 
+		// build fileId tag
+		Element file = doc.createElement("file");
+		file.setAttribute("id", fileId);
+		doc.appendChild(file);
+		// build chunk tag
+		// Element chunk=doc.createElement("chunk");
+		// file.appendChild(chunk);
+
+		// loop for build hash tag
+		for (int temp = 0; temp < hashlist.size(); temp++) {
+			Element hash = doc.createElement("hash");
+			Text hashtext = doc.createTextNode(hashlist.get(temp));
+			hash.appendChild(hashtext);
+			file.appendChild(hash);
+		}
+		// out put
+		try {
+			FileOutputStream fos = new FileOutputStream(fileId);
+			OutputStreamWriter outwriter = new OutputStreamWriter(fos);
+
+			callWriteXmlFile(doc, outwriter, "gb2312");
+			outwriter.close();
+			fos.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String args[]) throws Exception {
+		String str = "myfile.xml";
+		DomTest t = new DomTest();
+		ArrayList<String> hashlist = new ArrayList<String>();
+		hashlist.add("aaaa");
+		hashlist.add("bbbb");
+		hashlist.add("cccc");
+		hashlist.add("dddd");
+		// writeFileHash("myfile", hashlist);
+		t.writeFileHash(str, hashlist);
+	}
+}
